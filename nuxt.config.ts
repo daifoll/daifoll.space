192f7024a8f0d51a10ebd739d5f4aab16435bd1e
daifoll <kobykhnov.bl@gmail.com>
2025-08-01 15:21:21 +0700

    v0.1
    feat: Инициирован проект на Nuxt
    
    feat: Установлены необхолимые зависимости
    
    feat: Настроены некоторые конфиги

---
+++ nuxt.config.ts
@@ -0,0 +1,34 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  
+  /* Модули */
+  modules: [
+    '@nuxt/content',
+    '@nuxt/image',
+    '@nuxt/devtools',
+    '@nuxt/eslint',
+    '@nuxtjs/tailwindcss',
+    '@hypernym/nuxt-anime'
+  ],
+
+
+  content: {
+    // Здесь можно добавить дополнительные настройки модуля, например:
+    // highlight: {
+    //   theme: 'github-dark'
+    // }
+  },
+
+  /* Tailwind config module */
+  tailwindcss: {
+    
+  },
+
+  /* Anime.js config module */
+  anime: {
+    provide: true,
+    composables: true
+  }
+})
